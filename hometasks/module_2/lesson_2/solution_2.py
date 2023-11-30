c6566b2ba29824b018e0b981088fef2ec9fce1fe
Alyona Elinskaya <alyona.elinskaya@gmail.com>
2023-11-30 14:10:55 +0300

    solution_2

---
+++ hometasks/module_2/lesson_2/solution_2.py
@@ -0,0 +1,5 @@
+first_price = int(input("Введите первую цену:"))
+second_price = int(input("Введите вторую цену:"))
+third_price = int(input("Введите третью цену:"))
+result = (first_price + second_price + third_price)/3
+print(result)
